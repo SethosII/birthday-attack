430872c58f067363c2aecf1f96294bd84c940173
SethosII <paul.jaehne@gmx.de>
2016-01-12 14:02:49 +0100

    removed unused code

--- src/main.cu
+++ src/main.cu
@@ -10,24 +10,17 @@
 
 // CUDA header files
 #include <cuda_runtime.h>
-#include <curand_kernel.h>
 
 // own header files
 #include "sha256.h"
 #include "birthdayAttack.h"
 
-const int LENGHT = 10;
-const size_t SIZE_OF_RANDS = LENGHT * sizeof(double);
-
 typedef struct Handle {
 	bool gpu;bool help;bool verbose;
 } Handle;
 
-void fillRandomCPU(double* array, const int length);
-__global__ void fillRandomGPU(unsigned int seed, double* array,
-		const int length);
+void hashtestCPU();
 __global__ void hashtestGPU();
-void printArray(const double* array, const int length);
 void processParameters(Handle* handle, int argc, char* argv[]);
 
 int main(int argc, char* argv[]) {
@@ -35,70 +28,15 @@ int main(int argc, char* argv[]) {
 	handle = {.gpu = false, .help = false, .verbose = false};
 	processParameters(&handle, argc, argv);
 
-	double* randomNumbers = (double*) malloc(SIZE_OF_RANDS);
 
-	if (handle.gpu) {
-		double* d_randomNumbers;
-		cudaMalloc((void **) &d_randomNumbers, SIZE_OF_RANDS);
-
-		fillRandomGPU<<<1, 1>>>(time(NULL), d_randomNumbers, LENGHT);
-
-		cudaMemcpy(randomNumbers, d_randomNumbers, SIZE_OF_RANDS,
-				cudaMemcpyDeviceToHost);
-
-		cudaFree(d_randomNumbers);
-	} else {
-		fillRandomCPU(randomNumbers, LENGHT);
-	}
-
-	printArray(randomNumbers, LENGHT);
-
-	free(randomNumbers);
-
+#ifndef NDEBUG
 	if (handle.gpu) {
 		hashtestGPU<<<1, 1>>>();
 		cudaDeviceReset();
 	} else {
-#ifndef NDEBUG
-		testSha256LongInput();
-		testReduceSha256();
-#endif
-
-		unsigned int constantTextLength = 3;
-		unsigned char *constantText[] = { (unsigned char*) "This",
-				(unsigned char*) "a", (unsigned char*) "day." };
-		unsigned int stencilLength = 4;
-		unsigned char *stencil[] = { (unsigned char*) "is",
-				(unsigned char*) "was", (unsigned char*) "great",
-				(unsigned char*) "bad" };
-
-		unsigned int maximalLength = constantTextLength + stencilLength / 2;
-		for (int i = 0; i < constantTextLength; i++) {
-			maximalLength += stringLength(constantText[i]);
-		}
-		for (int i = 0; i < stencilLength / 2; i++) {
-			maximalLength +=
-					(stringLength(stencil[i * 2])
-							> stringLength(stencil[i * 2 + 1])) ?
-							stringLength(stencil[i * 2]) :
-							stringLength(stencil[i * 2 + 1]);
-		}
-		unsigned char combined[maximalLength];
-
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				sprintf((char*) combined, "%s %s %s %s %s", constantText[0],
-						stencil[i], constantText[1], stencil[2 + j],
-						constantText[2]);
-				printf("%s\n", combined);
-
-				unsigned char hash[4];
-				reducedHash(combined, hash, 1);
-				printHash(hash, 4);
-				printf("\n");
-			}
-		}
+		hashtestCPU();
 	}
+#endif
 
 	unsigned int dim = pow(2, 16);
 
@@ -130,56 +68,14 @@ int main(int argc, char* argv[]) {
 	cudaFree(d_hashs);
 }
 
-void fillRandomCPU(double* array, const int length) {
-	for (int i = 0; i < length; i++) {
-		array[i] = (double) rand() / RAND_MAX;
-	}
-}
-
-__global__ void fillRandomGPU(unsigned int seed, double* result,
-		const int lenght) {
-	int id = blockIdx.x * blockDim.x + threadIdx.x;
-	curandState_t state;
-	curand_init(seed, id, id, &state);
-	for (int i = 0; i < lenght; i++) {
-		result[i] = curand(&state);
-	}
+void hashtestCPU() {
+	testSha256LongInput();
+	testReduceSha256();
 }
 
 __global__ void hashtestGPU() {
-#ifndef NDEBUG
 	testSha256LongInput();
 	testReduceSha256();
-#endif
-
-	unsigned char hash[4];
-	unsigned char* combined;
-	combined = (unsigned char*) "This is a great day.";
-	printf("%s\n", combined);
-	reducedHash(combined, hash, 1);
-	printHash(hash, 4);
-	printf("\n");
-	combined = (unsigned char*) "This is a bad day.";
-	printf("%s\n", combined);
-	reducedHash(combined, hash, 1);
-	printHash(hash, 4);
-	printf("\n");
-	combined = (unsigned char*) "This was a great day.";
-	printf("%s\n", combined);
-	reducedHash(combined, hash, 1);
-	printHash(hash, 4);
-	printf("\n");
-	combined = (unsigned char*) "This was a bad day.";
-	printf("%s\n", combined);
-	reducedHash(combined, hash, 1);
-	printHash(hash, 4);
-	printf("\n");
-}
-
-void printArray(const double* array, const int length) {
-	for (int i = 0; i < length; i++) {
-		printf("array[%d] = %f\n", i, array[i]);
-	}
 }
 
 /*
