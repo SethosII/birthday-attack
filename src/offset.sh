afbb7e09325364de7a1383af9b4bafcc482ca0e6
SethosII <paul.jaehne@gmx.de>
2016-01-12 14:03:27 +0100

    added offset.sh
    for generation of arrays

---
+++ src/offset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Purpose: convert lines of file ($1) into one line and offset list
+
+text=""
+offsetList=""
+offset=0
+
+while read -r line
+do
+	text+="$line"
+	offsetList+="$offset, "
+	offset=$(echo "$(echo "$line" | wc -c) - 1 + $offset" | bc -l)
+done < "$1"
+offsetList+="$offset"
+
+echo $text
+echo $offsetList
